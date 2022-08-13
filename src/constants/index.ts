38c1208a256a538d306e233fa63a593aa44c36ce
Pablo Poggio <pablopoggiog@gmail.com>
2022-08-13 14:36:45 -0300

    Set up Infura as provider

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const INFURA_WEBSOCKETS_URL = "wss://mainnet.infura.io/ws/v3/";
+export const INFURA_PROJECT_ID = "ed948b163f0842cf86b3e1908a05f376";
+export const INFURA_NETWORK = "mainnet";
