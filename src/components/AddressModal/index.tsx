725087eb883887e5615334500bd0a7ff4266aae6
Pablo Poggio <pablopoggiog@gmail.com>
2022-08-13 13:52:44 -0300

    Populate modal and set basic styling

--- src/components/AddressModal/index.tsx
+++ src/components/AddressModal/index.tsx
@@ -1,15 +1,36 @@
+import { useState, ChangeEvent, useEffect } from "react";
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Title, Form, AddressInput, modalStyles } from "./styles";
 
 export const AddressModal = () => {
+  const [address, setAddress] = useState("");
+
+  const onAddressChange = ({
+    target: { value }
+  }: ChangeEvent<HTMLInputElement>) => {
+    setAddress(value);
+  };
+
+  useEffect(() => {
+    console.log("address", address);
+  }, [address]);
+
   return (
     <Modal
       key="address-modal"
       isOpen={true}
       onRequestClose={() => {}}
       ariaHideApp={false}
+      style={modalStyles}
     >
-      <Container>Address modal</Container>
+      <Title>Enter an address to track</Title>
+      <Form>
+        <AddressInput
+          placeholder="address"
+          value={address}
+          onChange={onAddressChange}
+        />
+      </Form>
     </Modal>
   );
 };
--- src/components/AddressModal/styles.ts
+++ src/components/AddressModal/styles.ts
@@ -1,3 +1,29 @@
 import styled from "styled-components";
+import { Styles as ModalStyles } from "react-modal";
 
-export const Container = styled.div``;
+export const Title = styled.h2``;
+
+export const Form = styled.form`
+  display: flex;
+  gap: 1em;
+`;
+
+export const AddressInput = styled.input`
+  border-radius: 0.5em;
+`;
+
+export const modalStyles: ModalStyles = {
+  content: {
+    maxWidth: "30em",
+    height: "11em",
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "1em",
+    boxSizing: "border-box",
+    color: "white",
+    backgroundColor: "#282c34",
+    borderRadius: "0.5em"
+  }
+};
