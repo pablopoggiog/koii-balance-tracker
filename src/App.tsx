241fe1b0fa74b71263bf512ee96324579ef985c0
Pablo Poggio <pablopoggiog@gmail.com>
2022-08-13 15:38:59 -0300

    Replace useTrack with TrackContext, and convert useTrack to just an interface to access it easily

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { AddressModal } from "src/components";
+import { useTrack } from "src/hooks";
 import { Container } from "./styles";
 
 const App = () => {
   const [isAddressModalOpen, setIsAddressModalOpen] = useState<boolean>(false);
+
+  useTrack();
 
   const openModal = () => setIsAddressModalOpen(true);
   const closeModal = () => setIsAddressModalOpen(false);
