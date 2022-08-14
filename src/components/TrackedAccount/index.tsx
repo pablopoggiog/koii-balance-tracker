7a0925f2cf2e36d080705a13597e380acb1d08cd
Pablo Poggio <pablopoggiog@gmail.com>
2022-08-13 21:06:48 -0300

    Improvements to TrackedAccount (token icons, styles, add last updated)

--- src/components/TrackedAccount/index.tsx
+++ src/components/TrackedAccount/index.tsx
@@ -1,24 +1,53 @@
 import { FC } from "react";
-import { Container, Address, BalancesContainer, TokenBalance } from "./styles";
 import { Account } from "src/types";
+import LinkLogo from "src/assets/link.svg";
+import UsdtLogo from "src/assets/usdt.svg";
+import DaiLogo from "src/assets/dai.svg";
+import {
+  Container,
+  Address,
+  BalancesContainer,
+  TokenBalance,
+  Icon,
+  Name,
+  LastUpdated
+} from "./styles";
 
 interface TrackedAccountProps {
   account: Account;
 }
 
+const TOKEN_ICONS = {
+  LINK: LinkLogo,
+  USDT: UsdtLogo,
+  DAI: DaiLogo
+};
+
 export const TrackedAccount: FC<TrackedAccountProps> = ({
   account: { address, balances }
 }) => {
+  const lastUpdated = `Last updated: ${new Date().toLocaleString()}`;
+  const getFormattedAmount = (amount: string) =>
+    new Intl.NumberFormat().format(Math.round(Number(amount)));
+
   return (
     <Container>
       <Address>{address}</Address>
       <BalancesContainer>
-        {balances.map((balance) => (
-          <TokenBalance>
-            {balance.symbol}: {balance.amount}
+        {balances.map((balance, index) => (
+          <TokenBalance key={index}>
+            <Name>
+              <Icon
+                alt={`${TOKEN_ICONS[balance.symbol]} icon`}
+                src={TOKEN_ICONS[balance.symbol]}
+              />
+              {balance.symbol}:
+            </Name>
+            <span>{getFormattedAmount(balance.amount)}</span>
           </TokenBalance>
         ))}
       </BalancesContainer>
+      <LastUpdated>{lastUpdated}</LastUpdated>
     </Container>
   );
 };
--- src/components/TrackedAccount/styles.ts
+++ src/components/TrackedAccount/styles.ts
@@ -6,10 +6,58 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   gap: 1em;
+  border-radius: 1em;
+  padding: 2em;
+  box-sizing: border-box;
+  max-width: 400px;
+  background-image: linear-gradient(
+    135deg,
+    rgba(255, 255, 255, 0.05),
+    rgba(255, 255, 255, 0.1)
+  );
+  backdrop-filter: blur(21px);
+  -webkit-backdrop-filter: blur(21px);
+  box-shadow: 0 8px 32px rgba(0, 0, 0, 0.36);
+  text-shadow: 1px 1px #32cfd1a7, -1px -1px #e1a8f0;
+  margin: auto;
 `;
 
-export const Address = styled.p``;
+export const Address = styled.p`
+  word-break: break-all;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+`;
 
-export const BalancesContainer = styled.p``;
+export const BalancesContainer = styled.div`
+  gap: 1em;
+  display: flex;
+  flex-direction: column;
+`;
 
-export const TokenBalance = styled.p``;
+export const TokenBalance = styled.div`
+  background-color: #e5c5ee;
+  padding: 1em;
+  border-radius: 1em;
+  font-weight: bold;
+  opacity: 0.8;
+  width: 10em;
+  justify-content: space-between;
+  display: flex;
+`;
+
+export const Name = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.3em;
+`;
+
+export const Icon = styled.img`
+  width: 1em;
+  height: 1em;
+`;
+
+export const LastUpdated = styled.p`
+  font-size: 0.5em;
+`;
